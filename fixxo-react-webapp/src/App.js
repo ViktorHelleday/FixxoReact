3b083bb2dea12df9487c4cab3795457f96d8c61c
Viktor Helleday <viktor.helleday@gmail.com>
2022-11-14 01:36:15 +0100

    Mainmenu before responsive design

--- fixxo-react-webapp/src/App.js
+++ fixxo-react-webapp/src/App.js
@@ -1,9 +1,36 @@
+import React from 'react';
 import './style.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import MainMenuSection from './sections/MainMenuSection';
+
+import HomeView from './views/HomeView';
+import ContactsView from './views/ContactsView';
+import CategoriesView from './views/CategoriesView';
+import ProductsView from './views/ProductsView';
+import SearchView from './views/SearchView';
+import CompareView from './views/CompareView';
+import WishlistView from './views/WishlistView';
+import ShoppingCartView from './views/ShoppingCartView';
+import NotFoundView from './views/NotFoundView';
+import FooterSection from './sections/FooterSection';
+
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/contacts" element={<ContactsView />} />
+        <Route path="/categories" element={<CategoriesView />} />
+        <Route path="/products" element={<ProductsView />} />
+        <Route path="/search" element={<SearchView />} />
+        <Route path="/compare" element={<CompareView />} />
+        <Route path="/wishlist" element={<WishlistView />} />
+        <Route path="/shoppingcart" element={<ShoppingCartView />} />
+        <Route path="*" element={<NotFoundView />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
